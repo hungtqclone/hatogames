452b3d0df56b64c24a9ee1fe357c8c478c0c1785
nhatduy2210 <tenduyhole@gmail.com>
2023-08-24 20:10:40 +0700

    map1new

---
+++ Assets/Scripts/Movebg.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movebg : MonoBehaviour
+{
+    public Camera mainCamera;
+    private Transform mainCameraTransform;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 spriteSize;
+
+    private void Start()
+    {
+        mainCameraTransform = mainCamera.transform;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteSize = spriteRenderer.bounds.size;
+    }
+
+    private void Update()
+    {
+        float cameraX = mainCameraTransform.position.x;
+        float backgroundX = transform.position.x;
+
+        float diff = cameraX - backgroundX;
+        int loops = Mathf.FloorToInt(diff / spriteSize.x);
+        float newPositionX = backgroundX + spriteSize.x * loops;
+
+        transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
+    }
+}
